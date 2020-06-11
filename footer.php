e88d81847692fc51b4c2590c604e294bc7f69e55
Samuele Legrenzi <samuele.legrenzi@welcomedigital.it>
2020-06-11 18:07:48 +0200

    wip

--- footer.php
+++ footer.php
@@ -8,25 +8,53 @@
  * @subpackage Sam_Theme
  * @since 1.0.0
  */
+    
     $template_directory = get_template_directory_uri();
-    $path_images = $template_directory . '/assets/images/';
+    $path_images = $template_directory . '/assets/images/'; 
+    $linkedin_url = get_field('linkedin_url', 'options');
+    $facebook_url = get_field('facebook_url', 'options');
+    $instagram_url = get_field('instagram_url', 'options');
+    $youtube_url = get_field('youtube_url', 'options');
+    $nl_contact_form_id = get_field('nl_contact_form_id', 'options');
 ?>
 
         </main>
         <!-- #site-footer -->
         <footer id="site-footer" class="page-footer" role="contentinfo">
+            <?php if ( $linkedin_url || $facebook_url || $instagram_url || $youtube_url || $nl_contact_form_id ) :?>
             <div class="pre-footer bg-light">
                 <div class="container">
                     <div class="row">
-                        <div class="col-12">
-                            <a href="#"><img class="social-icon" src="<?php echo $path_images; ?>linkedin@2x.png" alt="linkedin"></a>
-                            <a href="#"><img class="social-icon" src="<?php echo $path_images; ?>facebook@2x.png" alt="facebook"></a>
-                            <a href="#"><img class="social-icon" src="<?php echo $path_images; ?>instagram@2x.png" alt="instagram"></a>
-                            <a href="#"><img class="social-icon" src="<?php echo $path_images; ?>youtube@2x.png" alt="youtube"></a>
+                        <div class="col-12 col-lg-6 order-lg-2">
+                            <?php
+                                if($nl_contact_form_id){ 
+                                    echo do_shortcode( '[contact-form-7 id=' . $nl_contact_form_id . ']' ); 
+                                }
+                            ?>
                         </div>
+                        <div class="col-12 col-lg-6">
+                            
+                            <?php if($linkedin_url):?>
+                            <a href="<?php echo $linkedin_url; ?>"><img class="social-icon" src="<?php echo $path_images; ?>linkedin@2x.png" alt="linkedin"></a>
+                            <?php endif;?>
+
+                            <?php if($facebook_url):?>
+                            <a href="<?php echo $facebook_url; ?>"><img class="social-icon" src="<?php echo $path_images; ?>facebook@2x.png" alt="facebook"></a>
+                            <?php endif;?>
+
+                            <?php if($instagram_url):?>
+                            <a href="<?php echo $instagram_url; ?>"><img class="social-icon" src="<?php echo $path_images; ?>instagram@2x.png" alt="instagram"></a>
+                            <?php endif;?>
+
+                            <?php if($youtube_url):?>
+                            <a href="<?php echo $youtube_url; ?>"><img class="social-icon" src="<?php echo $path_images; ?>youtube@2x.png" alt="youtube"></a>
+                            <?php endif;?>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
+            <?php endif; ?>
             <div class="container">
                 <div class="row">
                     <div class="col col-md-6 col-lg-4">
